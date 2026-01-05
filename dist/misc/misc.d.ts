165baf17b8d84f6785154904bad1f44897feb4b5
NullPointerInclusion <jesseoghenevwegba@outlook.com>
2026-01-05 22:26:21 +0100

    build previous changes

--- dist/misc/misc.d.ts
+++ dist/misc/misc.d.ts
@@ -1,5 +1,7 @@
+import type { AnyFunction } from "../types/types.ts";
 import type { Deferred } from "./types.ts";
 export declare const noop: (...args: unknown[]) => null;
 export declare const createDeferred: <T>() => Promise<Deferred<T>>;
+export declare const setImmediate: <T extends AnyFunction>(callback: T, args?: unknown[]) => number, clearImmediate: (immediate: number) => null;
 export * from "./types.ts";
 //# sourceMappingURL=misc.d.ts.map
--- dist/misc/misc.js
+++ dist/misc/misc.js
@@ -14,5 +14,58 @@ export const createDeferred = async () => {
     });
     return deferred;
 };
+export const { setImmediate, clearImmediate } = (() => {
+    const immediateQueue = new Map();
+    const channel = new MessageChannel();
+    const dummyEntry = {
+        callback: () => { },
+        arguments: [],
+        canceled: false,
+    };
+    const drainQueue = () => {
+        const queue = [...immediateQueue.values()];
+        immediateQueue.clear();
+        awaitingDrain = false;
+        for (let i = 0; i < queue.length; ++i) {
+            const entry = queue[i];
+            if (!entry || entry.canceled)
+                continue;
+            try {
+                entry.callback(...entry.arguments);
+            }
+            catch (error) {
+                console.log("An error occured whilst executing an immediate callback.");
+                console.error(error);
+            }
+        }
+        return null;
+    };
+    let awaitingDrain = false;
+    let immediate = 0;
+    channel.port2.onmessage = drainQueue;
+    return {
+        setImmediate: (callback, args = []) => {
+            if (typeof callback !== "function")
+                throw new TypeError("Invalid callback.");
+            if (!Array.isArray(args))
+                throw new TypeError("Invalid callback arguments.");
+            const _immediate = immediate++;
+            immediateQueue.set(_immediate, {
+                callback,
+                arguments: args,
+                canceled: false,
+            });
+            if (!awaitingDrain) {
+                channel.port1.postMessage(null);
+                awaitingDrain = true;
+            }
+            return _immediate;
+        },
+        clearImmediate: (immediate) => {
+            (immediateQueue.get(immediate) || dummyEntry).canceled = true;
+            return null;
+        },
+    };
+})();
 export * from "./types.js";
 //# sourceMappingURL=misc.js.map
